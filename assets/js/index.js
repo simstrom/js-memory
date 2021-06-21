a11d57999c6a924b0fe9f07b2fac6f1275c5745f
Simon Nystrom <52610222+simstrom@users.noreply.github.com>
2021-06-21 13:21:21 +0200

    setup: game mechanics

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,55 @@
 import { shuffle } from "./utils.js";
 
 let cars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let clickable = true;
 let gameArea;
+let openCard;
 
 addEventListener("load", main);
 
 function main() {
-  removeEventListener("load", main);
-  gameArea = document.querySelector("#game-area");
-  createGameArea();
+    removeEventListener("load", main);
+    gameArea = document.querySelector("#game-area");
+    createGameArea();
+    gameArea.addEventListener("click", cardClick);
+}
+
+function cardClick(event) {
+    const TARGET = event.target;
+    if (TARGET.tagName.toLowerCase() != "img" || !clickable) return;
+
+    TARGET.classList.remove("hidden");
+
+    if (openCard === undefined) {
+        openCard = TARGET;
+    } else if (openCard.src != TARGET.src) {
+        clickable = false;
+        setTimeout(function () {
+            openCard.classList.add("hidden");
+            TARGET.classList.add("hidden");
+            openCard = undefined;
+            clickable = true;
+        }, 1000);
+    } else {
+        openCard = undefined;
+    }
 }
 
 function createGameArea() {
-  cars = cars.concat(cars);
-  cars = shuffle(cars);
+    cars = cars.concat(cars);
+    cars = shuffle(cars);
 
-  for (let index = 0; index < cars.length; index++) {
-    const CAR_NUMBER = cars[index];
+    for (let index = 0; index < cars.length; index++) {
+        const CAR_NUMBER = cars[index];
 
-    let card = document.createElement("div");
-    card.classList.add("card");
-    let img = document.createElement("img");
-    img.classList.add("hidden");
-    img.setAttribute("src", `assets/img/car-${CAR_NUMBER}.jpg`);
+        let card = document.createElement("div");
+        card.classList.add("card");
+        let img = document.createElement("img");
+        img.classList.add("hidden");
+        img.setAttribute("src", `assets/img/car-${CAR_NUMBER}.jpg`);
+        img.setAttribute("draggable", false);
 
-    gameArea.appendChild(card);
-    card.appendChild(img);
-  }
+        gameArea.appendChild(card);
+        card.appendChild(img);
+    }
 }
